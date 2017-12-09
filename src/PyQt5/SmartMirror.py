247f788fbd42514efa43c06bd04da38d06968f64
sa15Dawid <sa15b502@technikum-wien.at>
2017-12-09 16:59:15 +0100

    Structure and Openhab Window

--- src/PyQt5/SmartMirror.py
+++ src/PyQt5/SmartMirror.py
@@ -12,10 +12,15 @@ from PyQt5 import QtCore, QtGui
 
 api_url = 'http://api.openweathermap.org/data/2.5/weather?id=2761369&appid=8b0928c7fca2d41f21d5c5db7eb970c9&units=metric'
 forecast_url = 'http://api.openweathermap.org/data/2.5/forecast?id=2761369&appid=8b0928c7fca2d41f21d5c5db7eb970c9&units=metric'
-#id = '2761369'
-#app_id = '8b0928c7fca2d41f21d5c5db7eb970c9'
+# id = '2761369'
+# app_id = '8b0928c7fca2d41f21d5c5db7eb970c9'
 icon_url = 'http://openweathermap.org/img/w/'
 quote_url = 'https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&callback='
+
+font_L = QFont('Helvetica', 35, QFont.Bold)
+font_M = QFont('Helvetica', 25)
+font_S = QFont('Helvetica', 20)
+font_XS = QFont('Helvetica', 15)
 
 
 class MainWindow(QMainWindow):
@@ -24,29 +29,41 @@ class MainWindow(QMainWindow):
         self.central_widget = QStackedWidget()
         self.setCentralWidget(self.central_widget)
         start_widget = StartWindowWidget(self)
-        start_widget.changeButton.clicked.connect(self.start)
+        start_widget.WeatherWindowButton.clicked.connect(self.wetter)
         self.central_widget.addWidget(start_widget)
         start_widget.update1()
         start_widget.update2()
 
         p = self.palette()
         p.setColor(self.backgroundRole(), Qt.black)
+        p.setColor(QPalette.Foreground, Qt.white)
         self.setPalette(p)
 
-    def start(self):
+    def wetter(self):
         weather_widget = WeatherWindowWidget(self)
         self.central_widget.addWidget(weather_widget)
         self.central_widget.setCurrentWidget(weather_widget)
         weather_widget.MainWindowButton.clicked.connect(self.main)
+        weather_widget.OHWindowButton.clicked.connect(self.oh)
         weather_widget.update3()
 
     def main(self):
         start_widget = StartWindowWidget(self)
         self.central_widget.addWidget(start_widget)
         self.central_widget.setCurrentWidget(start_widget)
-        start_widget.changeButton.clicked.connect(self.start)
+        start_widget.WeatherWindowButton.clicked.connect(self.wetter)
+        start_widget.OHWindowButton.clicked.connect(self.oh)
         start_widget.update1()
         start_widget.update2()
+
+    def oh(self):
+        oh_widget = OpenHabWidget(self)
+        self.central_widget.addWidget(oh_widget)
+        self.central_widget.setCurrentWidget(oh_widget)
+        oh_widget.weatherButton.clicked.connect(self.wetter)
+        oh_widget.mainButton.clicked.connect(self.main)
+        # oh_widget.updatelist()
+        oh_widget.filllist()
 
     def keyPressEvent(self, QKeyEvent):
         if QKeyEvent.key() == Qt.Key_Esc:
@@ -59,6 +76,7 @@ class StartWindowWidget(QWidget):
 
         p = self.palette()
         p.setColor(self.backgroundRole(), Qt.black)
+        p.setColor(QPalette.Foreground, Qt.white)
         self.setPalette(p)
 
         # Label
@@ -68,41 +86,53 @@ class StartWindowWidget(QWidget):
         self.weather_icon = QLabel()
         self.quote_label = QLabel()
         self.quote_author_label = QLabel()
-        
-        
 
-        self.time_label.setFont(QFont("Vendera", 25, QFont.Bold))
-        self.time_label.setStyleSheet('color:white')
+        self.time_label.setFont(font_L)
+        self.date_label.setFont(font_M)
+        self.weather_label.setFont(font_L)
+        self.quote_label.setFont(font_S)
+        self.quote_author_label.setFont(font_XS)
 
-        self.date_label.setFont(QFont("Vendera", 25, QFont.Bold))
-        self.date_label.setStyleSheet('color:white')
+        self.quote_label.setMinimumWidth(1000)
+        self.quote_label.setMaximumWidth(1000)
+        self.quote_label.setWordWrap(True)
+        self.quote_label.setAlignment(Qt.AlignCenter)
 
-        self.weather_label.setStyleSheet('color: white')
-        self.quote_label.setStyleSheet('color: white')
-        self.quote_author_label.setStyleSheet('color: white')
+
+        # self.time_label.setStyleSheet('color: white')
+        # self.date_label.setStyleSheet('color: white')
+        # self.weather_label.setStyleSheet('color: white')
+        # self.quote_label.setStyleSheet('color: white')
+        # self.quote_author_label.setStyleSheet('color: white')
 
         # Button
-        self.changeButton = QPushButton('Change Window')
-
+        self.WeatherWindowButton = QPushButton('Weather Window')
+        self.OHWindowButton = QPushButton('OpenHab Window')
         # Struktur
-        #h = QHBoxLayout()
+        h = QHBoxLayout()
+        h1 = QHBoxLayout()
+        h2 = QHBoxLayout()
 
         v = QVBoxLayout()
 
-        v.addWidget(self.time_label)
-        #v.addStretch(1)
-        v.addWidget(self.weather_label)
-        v.addWidget(self.weather_icon)
-
-        # v.addStretch(1)
-
-        #v.addLayout(h)
+        h.addWidget(self.time_label)
+        h.addStretch(1)
+        h.addWidget(self.weather_icon)
+        h.addWidget(self.weather_label)
+        v.addLayout(h)
         v.addWidget(self.date_label)
+        v.addStretch(1)
 
-        v.addWidget(self.quote_label)
-        v.addWidget(self.quote_author_label)
-        #v.addStretch(1)
-        v.addWidget(self.changeButton)
+        h1.addStretch(1)
+        h1.addWidget(self.quote_label)
+        h1.addStretch(1)
+        v.addLayout(h1)
+        h2.addStretch(1)
+        h2.addWidget(self.quote_author_label)
+        h2.addStretch(1)
+        v.addLayout(h2)
+        v.addWidget(self.WeatherWindowButton)
+        v.addWidget(self.OHWindowButton)
 
         self.setLayout(v)
 
@@ -110,11 +140,11 @@ class StartWindowWidget(QWidget):
 
     def update1(self):
         time = QTime.currentTime().toString()
-        #print("Time: " + time)
+        # print("Time: " + time)
         self.time_label.setText(time)
 
         date = QDateTime.currentDateTime().toString("dddd " + "dd" + "." + "MMM" "yyyy")
-        #print("Datum" + date)
+        # print("Datum" + date)
         self.date_label.setText(date)
 
         threading.Timer(1.0, self.update1).start()
@@ -141,7 +171,7 @@ class StartWindowWidget(QWidget):
         self.quote_label.setText(quote)
 
         quote_author = rq[0]['title']
-        self.quote_author_label.setText(quote_author)
+        self.quote_author_label.setText("~"+quote_author)
 
         threading.Timer(60000.0, self.update2).start()
 
@@ -149,6 +179,7 @@ class StartWindowWidget(QWidget):
 class WeatherWindowWidget(QWidget):
     def __init__(self, parent=None):
         super(WeatherWindowWidget, self).__init__(parent)
+
 
         # Label
         self.weather_label = QLabel()
@@ -160,13 +191,22 @@ class WeatherWindowWidget(QWidget):
         self.temp_min_label = QLabel()
         self.temp_max_label = QLabel()
         self.description_label = QLabel()
-        
+
+        # List
         self.forecast_list = QListWidget()
 
-        self.setStyleSheet("""QListWidget{background: black;}""")
-
         # Eigenschaften
-        self.weather_label.setFont(QFont("Vendera", 45, QFont.Bold))
+        self.weather_label.setFont(font_L)
+        self.country_label.setFont(font_L)
+        self.city_label.setFont(font_L)
+
+        self.sunrise_label.setFont(font_S)
+        self.sunset_label.setFont(font_S)
+        self.temp_max_label.setFont(font_S)
+        self.temp_min_label.setFont(font_S)
+        self.description_label.setFont(font_S)
+        self.forecast_list.setFont(font_XS)
+
         self.weather_label.setStyleSheet('color: white')
         self.country_label.setStyleSheet('color: white')
         self.city_label.setStyleSheet('color: white')
@@ -175,13 +215,18 @@ class WeatherWindowWidget(QWidget):
         self.temp_min_label.setStyleSheet('color: white')
         self.temp_max_label.setStyleSheet('color: white')
         self.description_label.setStyleSheet('color: white')
-
+        self.forecast_list.setStyleSheet('color:white')
+        self.setStyleSheet("""QListWidget{background: black;}""")
 
         # Button
         self.MainWindowButton = QPushButton('MainWindow')
+        self.OHWindowButton = QPushButton('OpenHab Window')
 
         # Struktur
         h = QHBoxLayout()
+        h2 = QHBoxLayout()
+        h3 = QHBoxLayout()
+
         v = QVBoxLayout()
 
         h.addWidget(self.country_label)
@@ -189,19 +234,28 @@ class WeatherWindowWidget(QWidget):
         h.addStretch(1)
         h.addWidget(self.weather_icon)
         h.addWidget(self.weather_label)
-        v.addWidget(self.sunrise_label)
-        h.addStretch(1)
-        h.addWidget(self.description_label)
-        v.addWidget(self.sunset_label)
-        v.addWidget(self.temp_max_label)
-        h.addWidget(self.temp_min_label)
-        # v.addStretch(1)
+
         v.addLayout(h)
+
+        h2.addWidget(self.sunrise_label)
+        h2.addStretch(1)
+        h2.addWidget(self.description_label)
+        h2.addWidget(self.temp_max_label)
+
+        v.addLayout(h2)
+
+        h3.addWidget(self.sunset_label)
+        h3.addStretch(1)
+        h3.addWidget(self.temp_min_label)
+
+        # v.addStretch(1)
+        v.addLayout(h3)
         v.addStretch(1)
+        v.addWidget(self.forecast_list)
         v.addWidget(self.MainWindowButton)
+        v.addWidget(self.OHWindowButton)
         self.setLayout(v)
 
-    
     def update3(self):
         r = requests.get(api_url).json()
         temp_c_float = r['main']['temp']
@@ -220,19 +274,19 @@ class WeatherWindowWidget(QWidget):
         self.weather_icon.setPixmap(QPixmap(icon))
 
         country = r['sys']['country']
-        self.country_label.setText(country)
-        
+        self.country_label.setText(country+",")
+
         city = str(r['name'])
         self.city_label.setText(city)
-        
+
         timestamp1 = datetime.datetime.fromtimestamp(r['sys']['sunrise'])
         sunrise = timestamp1.strftime('%H:%M')
         self.sunrise_label.setText("Sunrise:" + sunrise)
-        
+
         timestamp2 = datetime.datetime.fromtimestamp(r['sys']['sunset'])
         sunset = timestamp2.strftime('%H:%M')
         self.sunset_label.setText("Sunset:" + sunset)
-        
+
         temp_max = str(r['main']['temp_max'])
         self.temp_max_label.setText("Temp. max:" + temp_max)
 
@@ -241,21 +295,104 @@ class WeatherWindowWidget(QWidget):
 
         description = r['weather'][0]['description']
         self.description_label.setText(description)
-        
+
         r = requests.get(forecast_url).json()
 
         count = r['cnt']
 
-        for i in range(0,count,8):
-
+        for i in range(0, count, 8):
             date = r['list'][i]['dt_txt']
             description = r['list'][i]['weather'][0]['description']
             temp_max = r['list'][i]['main']['temp_max']
             temp_min = r['list'][i]['main']['temp_min']
 
-            self.forecast_list.addItem('On %s "%s" Temp will be between %s an %s °C' % (date[:-8],description,temp_max,temp_min))
+            self.forecast_list.addItem(
+                'On %s "%s" Temp will be between %s and %s °C' % (date[:-8], description, temp_max, temp_min))
 
         threading.Timer(60000.0, self.update3).start()
+
+
+class OpenHabWidget(QWidget):
+    def __init__(self, parent=None):
+        super(OpenHabWidget, self).__init__(parent)
+
+        p = self.palette()
+        p.setColor(self.backgroundRole(), Qt.black)
+        p.setColor(QPalette.Foreground, Qt.white)
+        self.setPalette(p)
+
+        self.state = ''
+        self.name = ''
+
+        p = self.palette()
+        p.setColor(self.backgroundRole(), Qt.black)
+        self.setPalette(p)
+
+        # Label
+        self.items_list = QListWidget()
+
+        # # Eigenschaften
+        self.items_list.setFont(font_M)
+        self.items_list.setStyleSheet('color:white')
+        self.setStyleSheet("""QListWidget{background: black;}""")
+
+        # Button
+        self.mainButton = QPushButton('Main Window')
+        self.weatherButton = QPushButton('Weather Window')
+
+        # Struktur
+        v = QVBoxLayout()
+        v.addWidget(self.items_list)
+        v.addWidget(self.mainButton)
+        v.addWidget(self.weatherButton)
+
+        self.setLayout(v)
+
+    # def list(self):
+    #
+    #     r = requests.get('http://172.22.0.120:8080/rest/items').json()
+    #
+    #     count = len(r)
+    #
+    #     for i in range(0, count):
+    #
+    #         # print(len(r))
+    #         self.items_list.addItem('%s %s' % (self.name, self.state))
+
+    def filllist(self):
+
+        r = requests.get('http://172.22.0.120:8080/rest/items').json()
+        # r = self.updatelist()
+        count = len(r)
+
+        for i in range(0, count):
+
+            state2 = r[i]['state']
+            name2 = r[i]['name']
+
+            # print(state2)
+
+            if self.state != state2:
+                self.state = state2
+
+            if self.name != name2:
+                self.name = name2
+
+            self.items_list.addItem('%s %s' % (self.name, self.state))
+
+
+            # self.items_list.repaint()
+            # threading.Timer(1.0, self.filllist).start()
+
+    def updatelist(self):
+
+        r = requests.get('http://172.22.0.120:8080/rest/items').json()
+        count = len(r)
+
+        for i in range(0, count):
+            state2 = r[i]['state']
+
+        threading.Timer(1.0, self.updatelist).start()
 
 
 def main():
@@ -269,3 +406,5 @@ def main():
 
 if __name__ == '__main__':
     main()
+
+
